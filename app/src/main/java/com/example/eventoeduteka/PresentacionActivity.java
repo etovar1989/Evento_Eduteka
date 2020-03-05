2c5e800dc7a3bccd539746814393306215212fe3
etovar1989 <etovar1989@gmail.com>
2020-03-05 14:01:29 -0500

    Precentacion

--- app/src/main/java/com/example/eventoeduteka/PresentacionActivity.java
+++ app/src/main/java/com/example/eventoeduteka/PresentacionActivity.java
@@ -2,13 +2,43 @@ package com.example.eventoeduteka;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class PresentacionActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_presentacion );
+
+        progressBar=findViewById( R.id.progressBar );
+        progressBar.setVisibility( View.VISIBLE );
+
+        new Handler().postDelayed( new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences=getSharedPreferences( "preferenciasLogin", Context.MODE_PRIVATE );
+                boolean sesion=preferences.getBoolean( "sesion",false );
+                if(sesion){
+                    Intent intent=new Intent( getApplicationContext(), Menu.class );
+                    startActivity( intent );
+                    finish();
+                }else{
+                    Intent intent=new Intent( getApplicationContext(),MainActivity.class );
+                    startActivity( intent );
+                    finish();
+                }
+
+            }
+        }, 1000 );
+
     }
 }
